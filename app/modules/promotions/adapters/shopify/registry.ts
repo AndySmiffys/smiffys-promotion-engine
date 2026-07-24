92afa46de5d72bd6f97b8189e2c6799e228b2ed9
AndySmiffys <104154258+AndySmiffys@users.noreply.github.com>
2026-07-24 12:12:36 +0100

    Complete provider framework

--- app/modules/promotions/adapters/shopify/registry.ts
+++ app/modules/promotions/adapters/shopify/registry.ts
@@ -1,28 +1,16 @@
-import type { ShopifyDiscountNode } from "../../types/discount";
-import type { ShopifyPromotionProvider } from "./types";
+import { ProviderManager } from "./ProviderManager";
 import { bxgyProvider } from "./providers/bxgyProvider";
 import { orderProvider } from "./providers/orderProvider";
 import { productProvider } from "./providers/productProvider";
 import { shippingProvider } from "./providers/shippingProvider";
 import { unknownProvider } from "./providers/unknownProvider";
 
-const providers: ShopifyPromotionProvider[] = [
-  productProvider,
-  orderProvider,
-  shippingProvider,
-  bxgyProvider,
+export const shopifyProviderManager = new ProviderManager(
+  [
+    productProvider,
+    orderProvider,
+    shippingProvider,
+    bxgyProvider,
+  ],
   unknownProvider,
-];
-
-export function resolveShopifyPromotionProvider(
-  node: ShopifyDiscountNode,
-): ShopifyPromotionProvider {
-  return (
-    providers.find((provider) => provider.supports(node)) ??
-    unknownProvider
-  );
-}
-
-export function getShopifyPromotionProviders(): readonly ShopifyPromotionProvider[] {
-  return providers;
-}
+);
